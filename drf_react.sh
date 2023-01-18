f8702bab8ed97f1625be2b9e210c28606c693a8a
Sevag Aredjian <sevagared2002@gmail.com>
2023-01-18 08:05:29 -0500

    V1.1: Basic DRF/React installation

---
+++ drf_react.sh
@@ -0,0 +1,122 @@
+echo 'Which version of Django do you want: '
+read django_version
+mkdir backend && mkdir frontend && cd backend && python3 -m venv ./env && source env/bin/activate && pip install django==$django_version && pip install djangorestframework && pip install django-environ && pip freeze > requirements.txt
+echo 'Enter Django project name: '
+read django_proj_name
+django-admin startproject $django_proj_name .
+cat <<EOF >.gitignore
+__pycache__/
+*.py[cod]
+*$py.class
+
+*.so
+
+.Python
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+wheels/
+share/python-wheels/
+*.egg-info/
+.installed.cfg
+*.egg
+MANIFEST
+
+*.manifest
+*.spec
+
+pip-log.txt
+pip-delete-this-directory.txt
+
+htmlcov/
+.tox/
+.nox/
+.coverage
+.coverage.*
+.cache
+nosetests.xml
+coverage.xml
+*.cover
+*.py,cover
+.hypothesis/
+.pytest_cache/
+cover/
+
+*.mo
+*.pot
+
+*.log
+local_settings.py
+db.sqlite3
+db.sqlite3-journal
+
+instance/
+.webassets-cache
+
+.scrapy
+
+docs/_build/
+
+.pybuilder/
+target/
+
+.ipynb_checkpoints
+
+profile_default/
+ipython_config.py
+
+.pdm.toml
+
+__pypackages__/
+
+celerybeat-schedule
+celerybeat.pid
+
+*.sage.py
+
+.env
+.venv
+env/
+venv/
+ENV/
+env.bak/
+venv.bak/
+
+.spyderproject
+.spyproject
+
+.ropeproject
+
+/site
+
+.mypy_cache/
+.dmypy.json
+dmypy.json
+
+.pyre/
+.pytype/
+cython_debug/
+.DS_Store
+EOF
+./manage.py migrate
+cd $django_proj_name
+cat <<EOF >.env.template
+SK=secretkey
+DEBUG=debug_boolean
+EOF
+cat <<EOF >.env
+SK=
+DEBUG=True
+EOF
+cd ../../frontend
+echo 'Enter React project name: '
+read react_proj_name
+npx create-react-app .
---
+++ readme.md
@@ -0,0 +1,9 @@
+A bash script to generate a basic Django Rest Framework (DRF) and React project with other additional packages.
+For personal use.
+
+Next step(s):
+- Generate a sk in .env
+- Ask user is they want to use a customer DB (will be PostgreSQL)
+- Try to replace the values in settings.py by env('NAME_OF_VALUE')
+- Push to git as the 'init' commit
+- and more...
